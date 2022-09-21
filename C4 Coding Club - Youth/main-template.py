351df243c08adcc8546c90bcf65706d0ecbb066b
Lucas Lee <LucasDLee23@gmail.com>
2022-09-21 15:31:06 -0700

    Python notes

--- C4 Coding Club - Youth/main-template.py
+++ C4 Coding Club - Youth/main-template.py
@@ -5,6 +5,9 @@
 
 # Use the # to make a comment. Comments are notes in our code to remember what we wrote
 
+from cmath import sqrt
+
+
 print("Hello, World!") # notice it say Hello, world! in the black box called the console
 
 # Data Types
@@ -144,12 +147,12 @@ print(person)
 
 ### ACTIVITY: ###
 
-# Make a dictionary called school
-# Have the keys of name, population, and location
-# Name and location must be strings. Population is a number
-# You can make up your values
-# Print all of your keys and values
-# Update your school's name to something else and then print the school name only
+# 1) Make a dictionary called school
+# 2) Have the keys of name, population, and location
+# 3) Name and location be strings and population is a number
+# 4) You can make up your values
+# 5) Print all of your keys and values
+# 6) Update your school's name to something else and then print the school name only
 
 school = {
     "name": "UBC",
@@ -222,6 +225,10 @@ elif dice >= 20:
 else:
     print("Help")
 
+
+
+### Lecture 3 ###
+
 # and/or in if-statements
 
 if a == 2 and b == 3:
@@ -230,12 +237,161 @@ if a == 2 and b == 3:
 if a == 2 or b == 3:
     print("a is 2 and b is 3")
 
-
-
-### Lecture 3 ###
-
 # Loops #
+
+# A loop is a coding mechanism that lets us do certain things 
+# with our code without having to retype it
 
 # There are 2 types of loops: for and while loops
 
-# while-loops ##
+# while-loops
+# A while-loop runs "while" a condition if true
+# 1) A starting variable (usually a number)
+# 2) "while" keyword
+# 3) Ending condition
+# 4) Code within the while loop
+# 5) Some way to change our starting variable
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+
+# We can also have while-loops in another while-loop
+i = 0
+j = 0
+while i < 10:
+    print("Start new loop #" + str(i))
+    while j < 10:
+        print(j)
+        j = j + 1
+    j = 0 # reset j
+    i = i + 1 # increase i by 1
+
+# Stopping a while-loop midway
+i = 0
+while i < 5:
+    print(i)
+    if i == 2:
+        break # stops the while-loop
+    i += 1
+
+# Just like an if-statement, we can use "else" to do something after our loop runs
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("Stopped loop")
+
+# for-loops
+# A for-loop runs through a sequence of items
+# 1) A starting variable (like a list)
+# 2) "for" and "in" keywords
+# 3) Code within the loop
+
+cars = ["Ferrari", "Volvo", "Toyota", "Mercedes"]
+for x in cars:
+    print(x)
+
+city = "Richmond"
+for j in city:
+    print(j)
+
+# See the first while-loop we made on line 249
+# We can do the same thing with a for-loop using range()
+for k in range(10):
+    print(k)
+# range(number) is just a sequence of numbers
+
+for k in range(-5, 0): # can set a starting and ending parameter in range
+    print(k)
+    
+for k in range(-5, 5, 2): # can set a starting (-5) and ending (5) parameter in range as well as an increment value (2)
+    print(k)
+else:
+    print("Loop done")
+
+### ACTIVITY: ###
+
+# Using the "cars" list we made earlier, do the following:
+# Make a while-loop (not a for-loop) and print every item in the cars list
+# Some notes:
+# 1) len(cars) is the size of your list
+# 2) cars[0] is the beginning of your list
+
+i = 0
+while i < len(cars):
+    print(cars[i])
+    i = i + 1
+
+# Functions #
+
+# A function is a piece of code that we predefine so we can use it as many times as we want
+# Math: y = mx + b, y = 4x
+
+# 3 things to make a function:
+# 1) "def" (define) keyword
+# 2) The function's name
+# 3) Round () brackets
+# e.g. def myFunction()
+
+def sayHi():
+    print("Hi!")
+
+# To make the function work, we need to write the function name and brackets
+# This is called "calling a function"
+sayHi()
+sayHi()
+
+# Sometimes, we have a function that takes argument(s)
+
+def greeting(name):
+    print("Hi, " + name + "!")
+# An argument is some information needed to be put into the function so that it works
+# greeting() # gives an error because its expecting 1 argument
+greeting("Lucas")
+greeting("Kasie")
+
+# A function can take as many arguments as we want but for it to work,
+# it needs the same amount of items put into those arguments
+
+def add(x, y):
+    print(x + y)
+
+add(3, 5)
+add(7, 20)
+
+### ACTIVITY: ###
+
+# Make a function called daysGoneBy. This function will take 1 number as an argument. You may call your argument whatever you like
+# Within the function, make a for-loop. This for-loop's ending condition will be your function's argument (use range() for this)
+# Within the for-loop, print "It is day #" + str(your argument)
+# Test the function by calling it and see if it prints the number of days that corresponds to the number in your called function
+
+def daysGoneBy(days):
+    for x in range(days):
+        print("It is day #" + str(x))
+
+daysGoneBy(7)
+
+# Functions continued #
+
+# When we make a function, sometimes it can "return" us the result
+# This is useful as we can get very complicated calculations and by having "return" in a function
+# allows us to do those calculations and associate them to some variable afterwards
+# instead of putting all of those calculations on that variable
+
+def divide(x, y):
+    return x / y
+
+divide(3, 4) # notice how nothing is printed here
+print(divide(3, 4))
+
+divideFunction = divide(5, 2)
+print(divideFunction)
+
+
+
+### Lecture 4 ###
+
