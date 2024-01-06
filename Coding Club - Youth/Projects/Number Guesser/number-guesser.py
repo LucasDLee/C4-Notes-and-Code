775739af0c3f48d0e8fb9ddf31a571e70d81b3c2
Lucas Lee <lucasdlee23@gmail.com>
2024-01-05 17:45:27 -0800

    Web D&D intermediate notes

--- Coding Club - Youth/Projects/Number Guesser/number-guesser.py
+++ Coding Club - Youth/Projects/Number Guesser/number-guesser.py
@@ -26,41 +26,41 @@ def run_game():
         else:
             play_again = True
 
-# run_game()
+run_game()
 
-# Step 6
-import tkinter as tk
+# # Step 6
+# import tkinter as tk
 
-game = tk.Tk()
-game.title("Number Guesser")
+# game = tk.Tk()
+# game.title("Number Guesser")
 
-explanation = tk.Label(
-    text="Guess a number between 0 and 10 inclusive!"
-).pack()
-input_explanation = tk.Label(
-    text="What is your guess?"
-).pack()
-result = tk.Label()
-result.pack()
+# explanation = tk.Label(
+#     text="Guess a number between 0 and 10 inclusive!"
+# ).pack()
+# input_explanation = tk.Label(
+#     text="What is your guess?"
+# ).pack()
+# result = tk.Label()
+# result.pack()
 
-user_input = tk.Entry()
-user_input.pack()
+# user_input = tk.Entry()
+# user_input.pack()
 
-# Step 7
-def run_game2():
-    generate_number = random.randint(0,10) # 0, 1, 2, ..., 10
+# # Step 7
+# def run_game2():
+#     generate_number = random.randint(0,10) # 0, 1, 2, ..., 10
     
-    your_guess = int(user_input.get())
+#     your_guess = int(user_input.get())
     
-    if your_guess == generate_number:
-        result.config(text="You won! Your guess was " + str(your_guess) + " and so was the answer!")
-    else:
-        result.config(text="You lost. Your guess was " + str(your_guess) + " and the number was " + str(generate_number))
+#     if your_guess == generate_number:
+#         result.config(text="You won! Your guess was " + str(your_guess) + " and so was the answer!")
+#     else:
+#         result.config(text="You lost. Your guess was " + str(your_guess) + " and the number was " + str(generate_number))
 
-run_game = tk.Button(
-    text="Submit",
-    command=run_game2
-).pack()
+# run_game = tk.Button(
+#     text="Submit",
+#     command=run_game2
+# ).pack()
 
 
-game.mainloop()
+# game.mainloop()
